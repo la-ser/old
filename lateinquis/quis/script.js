af6e576b8eccc63698f70cab906564ff6b3f7f1f
la-ser <77540680+la-ser@users.noreply.github.com>
2022-03-28 09:59:48 +0200

    Update script.js

--- lateinquis/quis/script.js
+++ lateinquis/quis/script.js
@@ -57,7 +57,7 @@ function selectAnswer(e) {
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
         nextButton.classList.remove('hide')
     } else {
-        startButton.innerText = 'Restart Quiz'
+        startButton.innerText = 'Click to Restart Quiz'
         startButton.classList.remove('hide')
     }
 }
