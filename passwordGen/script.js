0c936c95abf049ab903b8c294867546be8413472
la-ser <77540680+la-ser@users.noreply.github.com>
2022-04-20 01:36:47 +0200

    Add files via upload

---
+++ passwordGen/script.js
@@ -0,0 +1,28 @@
+function getPassword(){
+    const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#$%^&*( )_+?><:{}[]'
+
+    let passwordLength = 16;
+    let password =  "";
+
+    for (let i=0; i<passwordLength; i++){
+        let randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber,randomNumber+1);
+    }
+    document.getElementById('password').value = password;
+}
+
+function copyPassword() {
+    /* Get the text field */
+    var copyText = document.getElementById("password");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+  
+    /* Alert the copied text */
+    //alert("Copied: " + copyText.value);
+    
+  }
